CyanogenMod/android_device_sony_nicki

nicki: Use CM libhealthd
Change-Id: I272cb2efb93ab8a07026513d95cd5aab12932e58

board/charger.mk
@@ -1,3 +1,3 @@
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
+ACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness

Sohamlad7/device_sony_nicki

board/charger.mk
@@ -1,3 +1,3 @@
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness